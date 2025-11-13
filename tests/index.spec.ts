e22160175446c0c1cb8690d582f66363e23fd53c
Atanas Atanasov <scriptex.bg@gmail.com>
2025-11-13 11:26:33 +0200

    Make alerts more prominent (#16)

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,7 @@
 import { existsSync, promises } from 'node:fs';
 import { resolve } from 'node:path';
 
+import * as cheerio from 'cheerio';
 import { minify } from 'html-minifier';
 import { test } from '@playwright/test';
 
@@ -37,19 +38,31 @@ test('find alerts', async ({ page }) => {
 		await page.locator('[name="username"]').fill(username.toString());
 		await page.locator('[name="pin"]').fill(password.toString());
 		await page.locator('[type="submit"]').click();
+		await page.selectOption('[name="type"]', 'all');
 
-		const table = page.locator('.table-responsive');
+		const searchIcon = page.locator('.dripicons-search');
 
-		results.push(await table.innerHTML());
+		await page.getByRole('button').filter({ has: searchIcon }).click();
+
+		const table = await page.locator('.table-responsive');
+		const html = await table.evaluate(el => el.innerHTML);
+		const $ = cheerio.load(html);
+
+		$('td:nth-child(3)').text(
+			$('td:nth-child(3)')
+				.text()
+				.replace(/(ВАРНА).*/u, '$1')
+		);
+
+		$('th:nth-child(2)').remove();
+		$('td:nth-child(2)').remove();
+
+		results.push($.html());
 
 		await page.locator('[href="/logout"]').click();
 	}
 
-	const messages = results.filter(item => !item.includes('Няма прекъсвания за обекти на клиента'));
-
-	if (messages.length > 0) {
-		await email(messages.join('<br><br>'));
-	}
+	await email(results.join('<br><br>'));
 
 	await writeFile(
 		filename,
